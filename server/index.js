d4b2c98d0b9b17d7eaf8244460be0bd7a5cfaf7b
Maire Chew <mairechew@gmail.com>
2017-08-24 09:57:51 -0600

    working files

--- server/index.js
+++ server/index.js
@@ -1,8 +1,20 @@
 var express = require('express')
 var server = express()
+var maire = require('./me.json')
 
-server.get('/about', function (request,response) {
-  response.send('Hello friends!')
+server.get('/', function (request,response) {
+  response.send('Go to /maire to find out more about me!')
 })
+
+server.get('/maire', function (request,response) {
+  response.json(maire)
+})
+
+server.get('*', function (request,response) {
+  response.status(400)
+  response.send('Sorry bud! Welcome to the wrong page')
+})
+
+
 
 server.listen(4000)
